3b05e59fdc46bc5767a12ca0b08e9ad739d83f7a
Alexander Yarkin <alexander.yarkin33@gmail.com>
2021-10-30 01:33:53 +0300

    Test output in main method

--- src/main/java/com/luxcampus/Program.java
+++ src/main/java/com/luxcampus/Program.java
@@ -2,6 +2,12 @@ package com.luxcampus;
 
 public class Program {
     public static void main(String[] args) {
+        Person person = new Person("Jack", 13);
+        try {
+            Reflection.callNoArgumentsMethods(person);
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
 
     }
 }
