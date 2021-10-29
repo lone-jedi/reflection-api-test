ed23c70e9d0d08375c0ecd8eee4601e583492c52
Alexander Yarkin <alexander.yarkin33@gmail.com>
2021-10-30 01:40:26 +0300

    Add new tests output into main()

--- src/main/java/com/luxcampus/Program.java
+++ src/main/java/com/luxcampus/Program.java
@@ -5,6 +5,9 @@ public class Program {
         Person person = new Person("Jack", 13);
         try {
             Reflection.callNoArgumentsMethods(person);
+            Reflection.printParentsAndInterfaces(Person.class);
+            Reflection.printPrivateMethods(Person.class);
+            ((Person)(Reflection.createInstance(Person.class))).sayHi();
         } catch (Throwable throwable) {
             throwable.printStackTrace();
         }
