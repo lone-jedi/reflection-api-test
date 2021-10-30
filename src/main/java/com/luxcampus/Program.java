98e11ab0495524e2e45be2a799cca0b607133692
Alexander Yarkin <alexander.yarkin33@gmail.com>
2021-10-30 10:27:44 +0300

    Add more methods calls

--- src/main/java/com/luxcampus/Program.java
+++ src/main/java/com/luxcampus/Program.java
@@ -4,10 +4,19 @@ public class Program {
     public static void main(String[] args) {
         Person person = new Person("Jack", 13);
         try {
+            System.out.println("Call methods without arguments:");
             Reflection.callNoArgumentsMethods(person);
+            System.out.println("Print patent class and interfaces:");
             Reflection.printParentsAndInterfaces(Person.class);
+            System.out.println("Print private methods:");
             Reflection.printPrivateMethods(Person.class);
+            System.out.println("Create instance of Person and call sayHi()");
             ((Person)(Reflection.createInstance(Person.class))).sayHi();
+            System.out.println("Print all final methods");
+            Reflection.printFinalMethods(person);
+            System.out.println("Clear private methods values");
+            Reflection.clearPrivateFieldsData(person);
+            System.out.println(person);
         } catch (Throwable throwable) {
             throwable.printStackTrace();
         }
